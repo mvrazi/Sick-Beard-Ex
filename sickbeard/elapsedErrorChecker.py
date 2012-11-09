f7960eb7ec59aa6c5de61322f8bd4a6adc5ee8ea
Dean Gardiner <gardiner91@gmail.com>
2012-11-10 10:27:36 +1300

    Added per-method min,max,avg calculations to [elapsedErrorChecker.py], [errorelapsed.tmpl]

--- sickbeard/elapsedErrorChecker.py
+++ sickbeard/elapsedErrorChecker.py
@@ -7,6 +7,7 @@ __author__ = 'Dean Gardiner'
 class ElapsedErrorChecker():
     def __init__(self):
         self.events = []
+        self.methods = {} # { <method>: [<events>] }
 
     def set(self, method, identifier = None):
         return ElapsedErrorCheckerState(self._buildName(method), identifier, datetime.now())
@@ -18,6 +19,9 @@ class ElapsedErrorChecker():
         state.elapsed = datetime.now() - state.startTime
         state.result = result
         self.events.insert(0, state)
+        if not self.methods.has_key(state.method):
+            self.methods[state.method] = ElapsedErrorCheckerMethod()
+        self.methods[state.method].append(state)
         logger.log("\"" + str(state.method) + "\" = " + str(state.elapsed))
 
     def _buildName(self, x):
@@ -26,10 +30,38 @@ class ElapsedErrorChecker():
         elif inspect.isclass(x.__class__):
             return x.__class__.__name__
 
+class ElapsedErrorCheckerMethod():
+    def __init__(self):
+        self.events = []
+
+    def append(self, state):
+        self.events.append(state)
+
+    def __len__(self):
+        return len(self.events)
+
+    def events_times(self):
+        times = []
+        for t in self.events:
+            times.append(t.elapsed.total_seconds())
+        return times
+
+    def events_min(self):
+        return min(self.events_times())
+
+    def events_max(self):
+        return max(self.events_times())
+
+    def events_avg(self):
+        times = self.events_times()
+        return sum(times) / len(times)
+
 class ElapsedErrorCheckerState():
     def __init__(self, method, identifier, startTime):
         self.method = method
         self.ident = identifier
         self.startTime = startTime
+        self.elapsed = None
+        self.result = None
 
 elapsedErrorChecker = ElapsedErrorChecker()
